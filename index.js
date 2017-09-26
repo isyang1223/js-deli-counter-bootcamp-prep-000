0f4f65c21e689eea61f34053b58d95ab5dd0226e
isyang1223 <isyang1223@gmail.com>
2017-09-26 21:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing(katzDeliLine){
+  katzDeliLine.shift();
+  katzDeliLine.pop();
+  return `Currently serving ${katzDeliLine}.`
+}
